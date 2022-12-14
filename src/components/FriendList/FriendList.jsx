c49dab47d1e6773ca5c4f5daa9a5e03add23444c
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-12-14 23:23:03 +0200

    adds styles

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
+import css from './FriendList.module.css'
 
 export default function FriendList({ friends }) {
     return (
-        <ul className="friend-list">
+        <ul className={css.friendList}>
             {friends.map(({ avatar, name, isOnline, id }) =>
                 <FriendListItem
                     key={id} 
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css'
 
 export default function Statistics({ title, stats }) {
   return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
-      <ul className="stat-list">
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li key={id} className="item">
-              <span className="label">{label}</span>
-              <span className="percentage">{percentage}%</span>
+            <li key={id} className={css.item}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
             </li>
           );
         })}
